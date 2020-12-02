88d46fd10fdbf17b176cc826885b416e4b88141d
Mtwod <maximewong@hotmail.com>
2020-12-02 14:23:23 +0100

    adds 'current_password' to update permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :avatar, :email, :password)}
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :avatar, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :avatar, :email, :password, :current_password) }
 
   end
 
