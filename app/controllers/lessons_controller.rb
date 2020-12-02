ec7d6394d26ecb226fe28209bf1fb90dc5305ee7
Rudyar <arthur.rudy.code@gmail.com>
2020-12-02 21:41:44 +0100

    Created def new and create in LessonsController + View new lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,4 +4,36 @@ class LessonsController < ApplicationController
 
   def show
   end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    @lesson.user_id = current_user
+    if @lesson.save
+      respond_to do |format|
+        format.html { redirect_to lesson(@lesson.id), notice: "Cours créé avec succès !" }
+        format.js { }
+      end
+    else
+      flash[:danger] = "Le cours n'a pas pu être créé"
+      render :new
+    end
+  end
+
+  def update
+    
+  end
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :description, :number_of_credit, :picture)
+    
+  end
+
+
+
 end
