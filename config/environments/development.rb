7a8c7cab736647a498a8bd0ba0dca4402b854fc1
Mtwod <maximewong@hotmail.com>
2020-12-02 09:34:32 +0100

    adds default url to action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Devise default mailer sender
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
