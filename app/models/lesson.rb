eba3e06f1cdec6f75fcb11fd28dcaeec57b0c92a
Mtwod <maximewong@hotmail.com>
2020-12-02 18:50:43 +0100

    adds methods not_teacher?, add_teacher_role and use them after creation

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,7 +4,17 @@ class Lesson < ApplicationRecord
   validates :title, uniqueness: { case_sensitive: false }
   validates :description, length: { in: 20...5000 }
   validates :number_of_credit, numericality: { grater_than_or_equal_to: 1 }
+  after_create :add_teacher_role, if: :not_teacher?
 
   has_one_attached :picture
-  
+
+  private
+
+  def not_teacher?
+    user.teacher? ? false : true
+  end
+
+  def add_teacher_role
+    user.assign_teacher_role
+  end
 end
