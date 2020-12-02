d9a8e15ad391c0a323d885723effa91ed1649f73
Mtwod <maximewong@hotmail.com>
2020-12-02 12:33:51 +0100

    adds autorisations, for devise, to params 'username' and 'avatar'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  # Allows to verify each requests with authenticity token sent to the application, with exception for added parameters to users (username, avatar)
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_params, if: :devise_controller?
+
+  private
+
+  # Configure the autorisation of params for controller devise only on sign up and update pages 
+  def configure_permitted_params
+
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :avatar, :email, :password)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :avatar, :email, :password)}
+
+  end
+
 end
