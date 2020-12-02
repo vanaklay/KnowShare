4d32e4372e7078f0d242c5fcb6508ecf33c1fee8
Mtwod <maximewong@hotmail.com>
2020-12-02 12:35:00 +0100

    adds 'avatar' to MODEL User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username,    presence: true,                     
-                          uniqueness: true
-                          
+  validates :username,  presence: true,
+                        uniqueness: true
+  
+  has_one_attached :avatar
+
 end
