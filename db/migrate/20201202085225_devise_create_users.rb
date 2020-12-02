c0e4debd5734508a79b28e4ed22b2035957283a9
Mtwod <maximewong@hotmail.com>
2020-12-02 10:07:25 +0100

    adds username and presence constraints in MODEL and TABLE

--- db/migrate/20201202085225_devise_create_users.rb
+++ db/migrate/20201202085225_devise_create_users.rb
@@ -3,6 +3,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+
+      t.string :username,           null: false
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
