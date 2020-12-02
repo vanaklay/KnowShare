0890bd98753f6bd5e6ead0c0bdf58ddcd11ab515
vanak <ven2kaze@gmail.com>
2020-12-02 09:40:43 +0100

    MailJet configuration added to dev and prod version

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['MAILJET_LOGIN'],
+  :password => ENV['MAILJET_PWD'],
+  :domain => 'gmail.com', 
+  :address => 'in-v3.mailjet.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
