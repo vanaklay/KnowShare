ade4df7b246f46a7d061ebd787dfa244913b9174
Mtwod <maximewong@hotmail.com>
2020-12-02 18:52:58 +0100

    edits mispellation and validation of title length

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,9 +1,9 @@
 class Lesson < ApplicationRecord
 
   validates_presence_of :title, :description, :number_of_credit
-  validates :title, uniqueness: { case_sensitive: false }
+  validates :title, uniqueness: { case_sensitive: false }, length: { minimum: 10 }
   validates :description, length: { in: 20...5000 }
-  validates :number_of_credit, numericality: { grater_than_or_equal_to: 1 }
+  validates :number_of_credit, numericality: { greater_than_or_equal_to: 1 }
   after_create :add_teacher_role, if: :not_teacher?
 
   has_one_attached :picture
