d123ec4edfa0e382744d48291bdd65afe09b4de0
Coline Brault <coline.brault@gmail.com>
2020-12-03 21:11:22 +0100

    commit before going to fix a bug in another branch

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,19 @@
 class BookingsController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!, only: [:create]
+  
+  def new
+  end 
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to(root_path)
+    else
+      flash[:danger] = "Votre réservation n'a pas abouti"
+      redirect_back(fallback_location: root_path)
+    end
+  end 
 
   def show
   end
@@ -12,5 +25,17 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+    @booking.destroy
+    respond_to do |format|
+      format.html {redirect_to @cart, notice: "Le cours a bien été annulé"}
+      format.json {head :no_content}
+      format.js {}
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :duration)
+  end
+
 end
