82edfb1562501904c96336816c5accd67f977fa7
Rudyar <arthur.rudy.code@gmail.com>
2020-12-04 09:14:07 +0100

    add 9 sample lessons on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+
   def index
-    @lessons = Lesson.all
+    if Lesson.all.length > 9
+      @lessons = Lesson.all.sample(9)
+    else
+      @lessons = Lesson.all
+    end
+
   end
+  
 end
