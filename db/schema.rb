dda5278832027b45c4e096fa9513d74c34a1e565
Mtwod <maximewong@hotmail.com>
2020-12-08 10:40:30 +0100

    fix in MODEL Booking the refund from method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_075641) do
+ActiveRecord::Schema.define(version: 2020_12_07_161740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_12_07_075641) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "lesson_id"
+    t.boolean "paid", default: false
     t.index ["lesson_id"], name: "index_bookings_on_lesson_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
