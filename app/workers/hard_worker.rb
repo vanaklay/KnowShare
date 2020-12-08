613040554fb3dd545d085d985592edf075617520
Mtwod <maximewong@hotmail.com>
2020-12-08 12:25:53 +0100

    edits worker file for scheduling test

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -1,7 +1,17 @@
 class HardWorker
   include Sidekiq::Worker
 
+  def initialize
+    @user = User.first
+    @amount = 100
+  end
+
   def perform()
-    p "********hi!*******"
+    p "*" * 100
+    p "********First_job*******"
+    Credit::Add.new(amount: @amount, user: @user)
+    p "Congratulations #{User.first}, you just earned #{@amount} credits ! You have now #{@user.personal_credit} credit(s)!"
+    p "********First_job*******"
+    p "*" * 100
   end
 end
