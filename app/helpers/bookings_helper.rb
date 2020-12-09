de256438a9afc62a5e6df6d176b189bef5d51762
Mtwod <maximewong@hotmail.com>
2020-12-09 18:55:51 +0100

    cleans bookings_helper and admin/bookings_controller

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -11,19 +11,4 @@ module BookingsHelper
     flash[:danger] = 'Vous ne pouvez pas réserver une séance avec vous-même !'
     redirect_back(fallback_location: root_path)
   end
-
-  def create_booking
-    if teacher?
-      prevent_teacher_booking
-    else
-      if @booking.save
-        flash[:success] = "Votre réservation a bien été prise en compte"
-        redirect_to(user_path(current_user))
-      else
-        flash[:danger] = "Votre réservation n'a pas pu aboutir"
-        redirect_back(fallback_location: root_path)
-      end
-    end
-  end
-
 end
