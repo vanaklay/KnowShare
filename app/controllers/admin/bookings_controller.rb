1e071d10a4a426314d40b34c08f77c8cefe2ebae
Coline Brault <coline.brault@gmail.com>
2020-12-10 16:07:19 +0100

    admin dashboard done

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -8,11 +8,20 @@ class Admin::BookingsController < Admin::FacadeController
   end 
   
   def destroy
-    @booking.destroy
-    respond_to do |format|
-      format.html { redirect_to admin_bookings_path, notice: "La réservation a bien été annulée" }
-      format.json { head :no_content }
-      format.js {}
+    if @booking.future?
+      Credit::Add.new(amount: @booking.price, user: @booking.student).call
+      BookingMailer.send_cancel_booking_email_to_student(booking: @booking).deliver_now
+      BookingMailer.send_cancel_booking_email_to_teacher(booking: @booking).deliver_now
+      @booking.create_schedule_after_cancelling
+      @booking.destroy
+      respond_to do |format|
+        format.html { redirect_to current_user, notice: "La réservation a bien été annulée" }
+        format.json { head :no_content }
+        format.js {}
+      end
+    else
+      flash[:danger] = "Impossible de supprimer une réservation passée"
+      redirect_back(fallback_location: root_path)
     end
   end
 
