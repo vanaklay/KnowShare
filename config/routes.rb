0f00131ba5aac3136244e8e2563aa48ed6968a9f
Mtwod <maximewong@hotmail.com>
2020-12-10 23:26:29 +0100

    edits path name into french, and tarifs_path to pricing_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,44 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get '/tarifs' => 'static_pages#pricing'
+  get '/tarifs' => 'static_pages#pricing', as: 'pricing'
   get '/terms' => 'static_pages#terms'
   get '/contact' => 'static_pages#contact'
   get '/team' => 'static_pages#team'
 
-  devise_for :users
-
-  resources :users do 
-    resources :schedules
-    resources :student_bookings, only: [:index]
-    resources :teacher_bookings, only: [:index]
-    resources :teacher_lessons, only: [:index]
-    resources :user_credit_orders, only: [:index]
-end
+  scope(path_names: { new: 'nouveau', edit: 'édition' }) do
   
-  
-  resources :lessons do
-    resources :teachers, only: [:show]
-    resources :bookings do 
-      resources :chatrooms, only: [:show, :create]
-    end 
-  end
-  
-  resources :messages, only:[:create]
-  resources :credit_orders, only:[:index, :new, :create]
-  resources :contacts, only:[:create, :new]
+    devise_for :users, path: 'utilisateurs'
 
-  # ActionCable
-  mount ActionCable.server => '/cable'
+    resources :users, path: 'utilisateurs' do
+      resources :schedules, path: 'horaires'
+      resources :student_bookings, only: [:index], path: 'réservations-côté-élève'
+      resources :teacher_bookings, only: [:index], path: 'réservations-côté-prof'
+      resources :teacher_lessons, only: [:index], path: 'leçons-prof'
+      resources :user_credit_orders, only: [:index], path: 'commandes-de-crédits'
+    end
 
-  # Admin 
-  namespace :admin do
-    root 'facade#index'
-    resources :users
-    resources :lessons
-    resources :bookings
-    resources :credit_orders, only:[:index]
+    
+    resources :lessons, path: 'leçons' do
+      resources :teachers, only: [:show], path: 'professeurs'
+      resources :bookings, path: 'réservations' do 
+        resources :chatrooms, only: [:show, :create], path: 'tchat'
+      end 
+    end
+    
+    resources :messages, only:[:create]
+    resources :credit_orders, only:[:index, :new, :create], path: 'commandes-de-crédits'
+    resources :contacts, only:[:create, :new], path: 'contacte-nous'
+
+    # ActionCable
+    mount ActionCable.server => '/cable'
+
+    # Admin 
+    namespace :admin, path: 'administrateur' do
+      root 'facade#index'
+      resources :users, path: 'utilisateurs'
+      resources :lessons, path: 'leçons'
+      resources :bookings, path: 'réservations'
+      resources :credit_orders, only:[:index], path: 'commandes-de-crédits'
+    end
   end
 end
