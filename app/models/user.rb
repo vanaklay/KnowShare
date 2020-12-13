bef133f34bcdf88d037c815d0831df1f6e082c2a
Mtwod <maximewong@hotmail.com>
2020-12-13 15:45:15 +0100

    removes extra comma in MODEL User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username,          presence: true,
-                                uniqueness: true,
+                                uniqueness: true
   has_many  :lessons,           dependent: :destroy
   has_many  :bookings,          dependent: :destroy
   has_many  :followed_lessons,  through: :bookings,
